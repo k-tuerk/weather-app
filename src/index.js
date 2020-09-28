0504838538b7ba34892cf8c8259daeb398ddc837
k-tuerk <ktuerk.4881@gmail.com>
2020-09-28 08:19:07 -0400

    Implement unit conversion

--- src/index.js
+++ src/index.js
@@ -1,45 +1,3 @@
-// //Getting todays date
-// let now = new Date();
-// let days = [
-//   `Sunday`,
-//   `Monday`,
-//   `Tuesday`,
-//   `Wednesday`,
-//   `Thursday`,
-//   `Friday`,
-//   `Saturday`,
-// ];
-// let day = days[now.getDay()];
-// let months = [
-//   `Janurary`,
-//   `Februrary`,
-//   `March`,
-//   `April`,
-//   `May`,
-//   `June`,
-//   `July`,
-//   `August`,
-//   `September`,
-//   `October`,
-//   `November`,
-//   `December`,
-// ];
-// let month = months[now.getMonth()];
-// let date = now.getDate();
-// let hour = now.getHours();
-// let minute = now.getMinutes();
-// let todayDate = document.querySelector(`h4#todayDate`);
-// let AMPM = `AM`;
-// if (hour > 12) {
-//   AMPM = `PM`;
-// } else {
-//   AMPM = `AM`;
-// }
-// if (minute < 10) {
-//   minute = `0${minute}`;
-// }
-// todayDate.innerHTML = `${day}, ${month} ${date} ${hour}:${minute} ${AMPM}`;
-
 function formatDate(timestamp) {
   let date = new Date(timestamp);
   let days = [
@@ -82,14 +40,16 @@ function showWeather(response) {
   let city = response.data.name;
   let maxTemp = Math.round(response.data.main.temp_max);
   let minTemp = Math.round(response.data.main.temp_min);
-  let temp = Math.round(response.data.main.temp);
   let description = response.data.weather[0].description;
   let humidityData = response.data.main.humidity;
   let windData = response.data.wind.speed;
+
+  celciusTemperature = Math.round(response.data.main.temp);
+
   let cityDisplay = document.querySelector("h1");
   cityDisplay.innerHTML = city;
   let currentTemp = document.querySelector("#currentTemp");
-  currentTemp.innerHTML = `${temp}`;
+  currentTemp.innerHTML = `${celciusTemperature}`;
   let celciusChange = document.querySelector("h3#tempHighLow");
   celciusChange.innerHTML = `${maxTemp}&degC/${minTemp}&degC`;
   let weatherDescription = document.querySelector("h4#weatherDefintion");
@@ -104,27 +64,27 @@ function showWeather(response) {
 }
 
 // Change metric
-// function celciusLink(event) {
-//   event.preventDefault();
-//   let celciusChange = document.querySelector("h3#tempOptions");
-//   let maxTemp = Math.round(response.data.main.temp_max);
-//   let minTemp = Math.round(response.data.main.temp_min);
-//   celciusChange.innerHTML = `${maxTemp}&degC/${minTemp}&degC`;
-//   console.log(response.data.main.temp_max);
-// }
-// function ferenheitLink(event) {
-//   event.preventDefault();
-//   let ferenheitChange = document.querySelector("h3#tempOptions");
-//   let maxTemp = Math.round(response.data.main.temp_max);
-//   let minTemp = Math.round(response.data.main.temp_min);
-//   ferenheitChange.innerHTML = `${maxTemp}&degF/${minTemp}&degF`;
-//   console.log(response.data.main.temp_max);
-// }
+function celciusLink(event) {
+  event.preventDefault();
+  let temp = document.querySelector("#currentTemp");
+  temp.innerHTML = celciusTemperature;
+  fClick.classList.remove(`active`);
+  cClick.classList.add(`active`);
+}
+function farenheitLink(event) {
+  event.preventDefault();
+  let farenheitTemp = Math.round((celciusTemperature * 9) / 5 + 32);
+  let temp = document.querySelector("#currentTemp");
+  temp.innerHTML = `${farenheitTemp}`;
+  cClick.classList.remove(`active`);
+  fClick.classList.add(`active`);
+}
+let celciusTemperature = null;
 
-// let cClick = document.querySelector("#Clink");
-// cClick.addEventListener("click", celciusLink);
-// let fClick = document.querySelector("#Flink");
-// fClick.addEventListener("click", ferenheitLink);
+let cClick = document.querySelector("#Clink");
+cClick.addEventListener("click", celciusLink);
+let fClick = document.querySelector("#Flink");
+fClick.addEventListener("click", farenheitLink);
 
 // Change Weather Based on Location
 function showPosition(position) {
@@ -138,15 +98,3 @@ function getCurrentLocation(event) {
 }
 let locationClick = document.querySelector(".currentLocation");
 locationClick.addEventListener("click", getCurrentLocation);
-
-// function showLocationTemp(event) {
-//   let city = response.data.name;
-//   let cityDisplay = document.querySelector("h1");
-//   let maxTemp = Math.round(response.data.main.temp_max);
-//   let minTemp = Math.round(response.data.main.temp_min);
-//   console.log(response.data.main.temp);
-//   console.log(position.coords.latitude);
-//   console.log(position.coords.longitude);
-//   let tempChange = document.querySelector("h3#tempOptions");
-//   tempChange.innerHTML = `${maxTemp}&degF/${minTemp}&degF`;
-// }
